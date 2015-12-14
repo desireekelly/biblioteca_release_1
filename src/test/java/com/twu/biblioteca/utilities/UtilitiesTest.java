e45c783bb4352eebfb06fd7d0644e109d520276d
Desiree Kelly <desireekelly@users.noreply.github.com>
2015-12-15 03:38:13 +1100

    Added UtilitiesTest

---
+++ src/test/java/com/twu/biblioteca/utilities/UtilitiesTest.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca.utilities;
+
+import org.junit.Test;
+
+/**
+ * Created by desiree on 15/12/2015.
+ */
+public class UtilitiesTest {
+
+    @Test
+    public void testDisplayFormattedBookList() throws Exception {
+
+    }
+}
