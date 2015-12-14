2431e7794649ed1a8f51b1f5c3b6217a36c43716
Desiree Kelly <desireekelly@users.noreply.github.com>
2015-12-14 17:37:18 +1100

    Updated BibliotecaApp and LibraryController

--- src/main/java/com/twu/biblioteca/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/biblioteca/BibliotecaApp.java
@@ -1,7 +1,13 @@
 package com.twu.biblioteca.biblioteca;
 
+import com.twu.biblioteca.library.*;
+
 class BibliotecaApp {
  public static void main(String[] args) {
-  System.out.println("Hello World!");
+
+     Library library = new LibraryImpl();
+     BorrowService borrowService = new BorrowService(library);
+     ReturnService returnService = new ReturnService(library);
+     LibraryController libraryController = new LibraryController(borrowService, returnService);
  }
 }
--- src/main/java/com/twu/biblioteca/library/LibraryController.java
+++ src/main/java/com/twu/biblioteca/library/LibraryController.java
@@ -4,16 +4,20 @@ import com.twu.biblioteca.book.Book;
 import com.twu.biblioteca.exceptions.BookNotBorrowable;
 import com.twu.biblioteca.exceptions.BookNotReturnable;
 
+import java.util.Scanner;
+
 /**
  * Created by desiree on 10/12/2015.
  */
 public class LibraryController {
     private BorrowService borrowService;
     private ReturnService returnService;
+    private Scanner input;
 
     public LibraryController(BorrowService borrowService, ReturnService returnService) {
         this.borrowService = borrowService;
         this.returnService = returnService;
+        this.input = new Scanner(System.in);
     }
 
     public boolean borrowBook(Book book) throws BookNotBorrowable {
@@ -23,4 +27,6 @@ public class LibraryController {
     public boolean returnBook(Book book) throws BookNotReturnable {
         return returnService.returnBook(book);
     }
+
+
 }
