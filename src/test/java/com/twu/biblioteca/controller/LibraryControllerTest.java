fb6bfce1297a6fb39d22d79bf4d637476ebe08d4
Desiree Kelly <desireekelly@users.noreply.github.com>
2015-12-16 01:00:56 +1100

    Updated LibraryController Test

--- src/test/java/com/twu/biblioteca/controller/LibraryControllerTest.java
+++ src/test/java/com/twu/biblioteca/controller/LibraryControllerTest.java
@@ -8,8 +8,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 /**
