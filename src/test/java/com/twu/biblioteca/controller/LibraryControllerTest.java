121b9d6603e52ef72551a19debe6805b9b5a824f
Desiree Kelly <desireekelly@users.noreply.github.com>
2016-01-02 21:21:50 +1100

    Updated LibraryControllerTest

--- src/test/java/com/twu/biblioteca/controller/LibraryControllerTest.java
+++ src/test/java/com/twu/biblioteca/controller/LibraryControllerTest.java
@@ -26,10 +26,6 @@ public class LibraryControllerTest {
     private LibraryController libraryController;
     private User user;
     private BorrowableItem item;
-    private LibraryController libraryControllerMock;
-
-    private static final String BOOK_TITLE = "Java 101";
-    private static final String MOVIE_TITLE = "The Matrix";
 
     @Before
     public void setUp() throws Exception {
@@ -40,7 +36,6 @@ public class LibraryControllerTest {
         libraryController = new LibraryController(library, borrowService, returnService, loginService);
         user = mock(User.class);
         item = mock(BorrowableItem.class);
-        libraryControllerMock = mock(LibraryController.class);
     }
 
     @Test
@@ -62,96 +57,5 @@ public class LibraryControllerTest {
         when(loginService.loginUser("123-4566", "f8kf93jd")).thenReturn(user);
         assertEquals(libraryController.loginUser("123-4566", "f8kf93jd"), user);
         verify(loginService, times(1)).loginUser("123-4566", "f8kf93jd");
-    }
-
-    @Test
-    public void testAvailableBooksIsEmpty() throws Exception {
-        when(libraryControllerMock.availableBooksIsEmpty()).thenReturn(true);
-        assertTrue(libraryControllerMock.availableBooksIsEmpty());
-        verify(libraryControllerMock, times(1)).availableBooksIsEmpty();
-    }
-
-    @Test
-    public void testBorrowedBooksIsEmpty() throws Exception {
-        when(libraryControllerMock.borrowedBooksIsEmpty()).thenReturn(true);
-        assertTrue(libraryControllerMock.borrowedBooksIsEmpty());
-        verify(libraryControllerMock, times(1)).borrowedBooksIsEmpty();
-    }
-
-    @Test
-    public void testGetAvailableBooksSize() throws Exception {
-        when(libraryControllerMock.getAvailableBooksSize()).thenReturn(4);
-        assertEquals(libraryControllerMock.getAvailableBooksSize(), 4);
-        verify(libraryControllerMock, times(1)).getAvailableBooksSize();
-    }
-
-    @Test
-    public void testGetBorrowedBooksSize() throws Exception {
-        when(libraryControllerMock.getBorrowedBooksSize()).thenReturn(4);
-        assertEquals(libraryControllerMock.getBorrowedBooksSize(), 4);
-        verify(libraryControllerMock, times(1)).getBorrowedBooksSize();
-    }
-
-    @Test
-    public void testAvailableMoviesIsEmpty() throws Exception {
-        when(libraryControllerMock.availableMoviesIsEmpty()).thenReturn(true);
-        assertTrue(libraryControllerMock.availableMoviesIsEmpty());
-        verify(libraryControllerMock, times(1)).availableMoviesIsEmpty();
-    }
-
-    @Test
-    public void testBorrowedMoviesIsEmpty() throws Exception {
-        when(libraryControllerMock.borrowedMoviesIsEmpty()).thenReturn(true);
-        assertTrue(libraryControllerMock.borrowedMoviesIsEmpty());
-        verify(libraryControllerMock, times(1)).borrowedMoviesIsEmpty();
-    }
-
-    @Test
-    public void testGetAvailableMoviesSize() throws Exception {
-        when(libraryControllerMock.getAvailableMoviesSize()).thenReturn(4);
-        assertEquals(libraryControllerMock.getAvailableMoviesSize(), 4);
-        verify(libraryControllerMock, times(1)).getAvailableMoviesSize();
-    }
-
-    @Test
-    public void testGetBorrowedMoviesSize() throws Exception {
-        when(libraryControllerMock.getBorrowedMoviesSize()).thenReturn(4);
-        assertEquals(libraryControllerMock.getBorrowedMoviesSize(), 4);
-        verify(libraryControllerMock, times(1)).getBorrowedMoviesSize();
-    }
-
-    @Test
-    public void testCheckoutBookReturnsTitle() throws Exception {
-        when(libraryControllerMock.checkoutBook(1)).thenReturn(BOOK_TITLE);
-        assertEquals(libraryControllerMock.checkoutBook(1), BOOK_TITLE);
-        verify(libraryControllerMock, times(1)).checkoutBook(1);
-    }
-
-    @Test
-    public void testCheckoutMovieReturnsName() throws Exception {
-        when(libraryControllerMock.checkoutMovie(1)).thenReturn(MOVIE_TITLE);
-        assertEquals(libraryControllerMock.checkoutMovie(1), MOVIE_TITLE);
-        verify(libraryControllerMock, times(1)).checkoutMovie(1);
-    }
-
-    @Test
-    public void testCheckinBookReturnsTitle() throws Exception {
-        when(libraryControllerMock.checkinBook(1)).thenReturn(BOOK_TITLE);
-        assertEquals(libraryControllerMock.checkinBook(1), BOOK_TITLE);
-        verify(libraryControllerMock, times(1)).checkinBook(1);
-    }
-
-    @Test
-    public void testCheckinMovieReturnsName() throws Exception {
-        when(libraryControllerMock.checkinMovie(1)).thenReturn(MOVIE_TITLE);
-        assertEquals(libraryControllerMock.checkinMovie(1), MOVIE_TITLE);
-        verify(libraryControllerMock, times(1)).checkinMovie(1);
-    }
-
-    @Test
-    public void testItemsCheckedOutByCustomersIsEmpty() throws Exception {
-        when(libraryControllerMock.itemsCheckedOutByCustomersIsEmpty()).thenReturn(true);
-        assertTrue(libraryControllerMock.itemsCheckedOutByCustomersIsEmpty());
-        verify(libraryControllerMock, times(1)).itemsCheckedOutByCustomersIsEmpty();
     }
 }
