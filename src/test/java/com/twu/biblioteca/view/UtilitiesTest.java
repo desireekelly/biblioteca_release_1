50244a628223fcd1b55f195e2f32f9040092508c
Desiree Kelly <desireekelly@users.noreply.github.com>
2016-01-08 18:42:32 +1100

    Refactoring

--- src/test/java/com/twu/biblioteca/view/UtilitiesTest.java
+++ src/test/java/com/twu/biblioteca/view/UtilitiesTest.java
@@ -5,6 +5,7 @@ import com.twu.biblioteca.model.Book;
 import com.twu.biblioteca.model.BorrowableItem;
 import com.twu.biblioteca.model.Movie;
 import com.twu.biblioteca.model.User;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -23,24 +24,29 @@ import static org.junit.Assert.assertEquals;
  */
 public class UtilitiesTest {
 
+    private List<BorrowableItem> items;
+
+    @Before
+    public void setUp() throws Exception {
+        items = new ArrayList<BorrowableItem>();
+    }
+
     @Test
     public void testFormatBookList() throws Exception {
-        List<Book> books = new ArrayList<Book>();
-        books.add(new Book("Java 101", "Joe Bloggs", 1990));
-        books.add(new Book("PHP 101", "Mary Jane", 2005));
+        items.add(new Book("Java 101", "Joe Bloggs", 1990));
+        items.add(new Book("PHP 101", "Mary Jane", 2005));
         assertEquals("0               Java 101        Joe Bloggs      1990           \n" +
                         "1               PHP 101         Mary Jane       2005           \n",
-                Utilities.formatBookList(books).toString());
+                Utilities.formatBooks(items).toString());
     }
 
     @Test
     public void testDisplayFormatMovieList() throws Exception {
-        List<Movie> movies = new ArrayList<Movie>();
-        movies.add(new Movie("The Matrix", 1999, "The Wachowski Brothers", "10"));
-        movies.add(new Movie("Inception", 2010, "Christopher Nolan", "8"));
+        items.add(new Movie("The Matrix", 1999, "The Wachowski Brothers", "10"));
+        items.add(new Movie("Inception", 2010, "Christopher Nolan", "8"));
         assertEquals("0                    The Matrix                     1999                 The Wachowski Brothers              10                  \n" +
                         "1                    Inception                      2010                 Christopher Nolan                   8                   \n",
-                Utilities.formatMovieList(movies).toString());
+                Utilities.formatMovies(items).toString());
     }
 
     @Test
